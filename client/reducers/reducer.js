dec136942f1db91e08a76e6be2d16e2596983514
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-03-03 23:23:14 -0400

    Putting combined reducers

--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -1,1 +1,9 @@
 import { createStore } from 'redux';
+import { combineReducers } from 'redux-immutable';
+import { reducer as textReducer } from './text/reducer';
+
+const rootReducer = combineReducers({textReducer});
+
+const store = createStore(rootReducer);
+
+export default store;
--- client/reducers/text/reducer.js
+++ client/reducers/text/reducer.js
@@ -17,3 +17,5 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 }
+
+export default reducer;
