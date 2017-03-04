bbf59b4aa1f74906b27148ca690f063d779e22b4
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-03-03 23:18:41 -0400

    Adding reducer logic

---
+++ client/reducers/text/actions.js
@@ -0,0 +1,7 @@
+export const actions = {
+  ADD_TEXT: "ADD_TEXT",
+  add: (textData) => ({
+      type: actions.ADD_TEXT,
+      textData
+  })
+}
---
+++ client/reducers/text/reducer.js
@@ -0,0 +1,19 @@
+import actions from './actions';
+import Immutable from 'immutable';
+
+const initialState = Immutable.fromJS({
+  textData: {
+    content: "Consectetur rerum aliquid non tenetur ducimus sunt corporis? Dolore quisquam ea tempora sequi ea! Cum placeat sit a ut ad. Aliquid accusamus veritatis vitae delectus corrupti! Voluptas numquam dolor eveniet!2",
+    leftToRight: true,
+    language: "Latin"
+  }
+});
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case actions.ADD_TEXT:
+      return state.set('textData',action.textData);
+    default:
+      return state;
+  }
+}
