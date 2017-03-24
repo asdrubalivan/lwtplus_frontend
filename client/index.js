7225f3af3db33a52bb4e99aaeaa3aebfd8adec41
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-03-23 20:02:26 -0400

    Importing react router

--- client/index.js
+++ client/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import store from './reducers/reducer';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={browserHistory}>
+            <Route path="/" component={App} />
+        </Router>
     </Provider>
     , document.getElementById('root'));
