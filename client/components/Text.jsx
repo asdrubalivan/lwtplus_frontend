a1aee69cf403752aca163aab40d93e12c7393143
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-03-24 21:53:35 -0400

    Adding properties to Text.jsx

--- client/components/Text.jsx
+++ client/components/Text.jsx
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
 
 class Text extends Component {
+    static defaultProps = {
+        knownWords: [
+            {
+                word: "Amet",
+                id: 1,
+                status: 2,
+            },
+            {
+                word: "soluta",
+                id: 2,
+                status: 1,
+            },
+            {
+                word: "atque optio",
+                id: 4,
+                status: 3
+            }
+        ]
+    };
     render() {
         return (
-            <div><span style={styles.known}>This</span> is my text</div>
+            <div>
+                Amet eligendi soluta similique atque optio. Aspernatur repudiandae ullam eveniet sapiente ab. Natus recusandae praesentium sit obcaecati ab incidunt ducimus nam, quas incidunt id? Ipsam porro animi nisi impedit quis?
+                
+            </div>
         );
     }
 }
