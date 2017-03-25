d474f3ab9ffa1df12611192e572dbd1aed10a7dd
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-03-24 21:23:48 -0400

    Adding Text class

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import Text from './Text.jsx';
 
 class App extends Component {
     render() {
       return ( 
         <div>
             <h1>Hola Mundo</h1>
+            <div>Y ahora tenemos un texto
+                <Text></Text>
+            </div>
         </div>
       );
     }
