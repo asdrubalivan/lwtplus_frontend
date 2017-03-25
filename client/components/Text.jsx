87d94b56177cbb1731429db4b53c7f0cbddc1ee9
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-03-24 21:30:49 -0400

    Adding styling for known and unknown words

--- client/components/Text.jsx
+++ client/components/Text.jsx
@@ -3,9 +3,21 @@ import React, {Component} from 'react';
 class Text extends Component {
     render() {
         return (
-            <div>This is my text</div>
+            <div><span style={styles.known}>This</span> is my text</div>
         );
     }
 }
+
+const styles = {
+    known: {
+        backgroundColor: 'green'
+    },
+    learning: {
+        backgroundColor: 'yellow',
+    },
+    unknown: {
+        backgroundColor: 'red'
+    }
+};
 
 export default Text;
