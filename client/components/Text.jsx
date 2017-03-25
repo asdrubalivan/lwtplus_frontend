54c9aa00aa0bc153a6330a915d3e26fc2f555c7d
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-03-24 22:50:55 -0400

    Adding WordDivider. It still needs unit tests

--- client/components/Text.jsx
+++ client/components/Text.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Word from './Word.jsx';
+import WordDivider from '../helpers/WordDivider';
 
 class Text extends Component {
     static defaultProps = {
@@ -18,9 +20,30 @@ class Text extends Component {
                 id: 4,
                 status: 3
             }
-        ]
+        ],
+        text: `
+                Amet eligendi soluta similique atque optio. Aspernatur repudiandae ullam eveniet sapiente ab. Natus recusandae praesentium sit obcaecati ab incidunt ducimus nam, quas incidunt id? Ipsam porro animi nisi impedit quis?
+                
+        `,
+        splitChars: false,
     };
+
+    get divider() {
+        return new WordDivider(this.props.text, false, this.props.splitChars, /[.!?:;]/g);
+    }
+
+    /*
+     * Algorithm
+     * 1) Divide the words in n
+     * 2) For each word in n
+     * 3) Have a hash and save the location of the words
+     * 4) Apply properties to words
+     * Notes: Maybe saving pairs of words too for accelerating search of pairs?
+     *
+        * */
     render() {
+        console.log(this.divider.sentences);
+        console.log(this.divider.words);
         return (
             <div>
                 Amet eligendi soluta similique atque optio. Aspernatur repudiandae ullam eveniet sapiente ab. Natus recusandae praesentium sit obcaecati ab incidunt ducimus nam, quas incidunt id? Ipsam porro animi nisi impedit quis?
---
+++ client/helpers/WordDivider.js
@@ -0,0 +1,21 @@
+export default class WordDivider {
+    constructor(text, removeSpaces, splitEachChar, regexSplitSentences) {
+        this.text = text;
+        this.removeSpaces = removeSpaces;
+        this.splitEachChar = splitEachChar;
+        this.regexSplitSentences = regexSplitSentences;
+    }
+
+    get sentences() {
+        let unprocessed = this.text.split(this.regexSplitSentences);
+        return unprocessed.map((txt) => txt.trim()).filter((txt) => txt);
+    }
+
+    get words() {
+        if (this.splitEachChar) {
+            return this.text.split('');
+        } else {
+            return this.text.match(/\b(\w+)\b/g);
+        }
+    }
+}
