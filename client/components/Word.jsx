3195fb2e6a199730f720523909bd21bad8a88d3d
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2017-04-01 22:04:57 -0400

    Changing Word from Component to PureComponent

--- client/components/Word.jsx
+++ client/components/Word.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 
-class Word extends Component {
+class Word extends PureComponent {
     //Props
     //word id
     //word status
